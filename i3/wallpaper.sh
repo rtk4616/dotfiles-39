d3ef9b142b8c91793edd680067bc6732b6fa1ec6
Jeff Langston <jrlangston@gmail.com>
2019-05-22 15:25:47 -0600

    link dotfiles change and i3 nitrogen

---
+++ i3/wallpaper.sh
@@ -0,0 +1,4 @@
+while true; do
+      nitrogen --set-zoom --random /usr/share/backgrounds
+      sleep 5m
+done
--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -8,8 +8,12 @@ rm -rf ~/.tmux.conf
 ln -s .tmux.conf ~/.tmux.conf;
 rm -rf ~/.tmuxline.conf
 ln -s .tmuxline.conf ~/.tmuxline.conf;
+mkdir -P ~/.config/pgcli
 rm -rf ~/.config/pgcli/config
 ln -s .pgclirc ~/.config/pgcli/config;
 rm -rf ~/.myclirc
 ln -s .myclirc ~/.myclirc
+mkdir -P ~/.config/i3
+rm -rf ~/.config/i3/config
+ln -s i3/config ~/.config/i3/config
 
