dc1ec5c5e4a50e317f9b2ca991deb3de3b1933f8
Jeff Langston <jrlangston@gmail.com>
2019-07-18 16:22:39 -0600

    Indempotent link files

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -1,19 +1,11 @@
-#!/bin/sh
+#!/bin/zsh
 mkdir -p ~/.config
-rm -rf ~/.config/nvim
-ln -s nvim ~/.config/nvim;
-rm -rf ~/.zshrc
-ln -s .zshrc ~/.zshrc;
-rm -rf ~/.tmux.conf
-ln -s .tmux.conf ~/.tmux.conf;
-rm -rf ~/.tmuxline.conf
-ln -s .tmuxline.conf ~/.tmuxline.conf;
-mkdir -P ~/.config/pgcli
-rm -rf ~/.config/pgcli/config
-ln -s .pgclirc ~/.config/pgcli/config;
-rm -rf ~/.myclirc
-ln -s .myclirc ~/.myclirc
-mkdir -P ~/.config/i3
-rm -rf ~/.config/i3/config
-ln -s i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/nvim ~/.config/nvim;
+ln -sf ~/dotfiles/.zshrc ~/.zshrc;
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf;
+ln -sf ~/dotfiles/.tmuxline.conf ~/.tmuxline.conf;
+ln -sf ~/dotfiles/.pgclirc ~/.config/pgcli/config;
+ln -sf ~/dotfiles/.myclirc ~/.myclirc
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/.asdfrc ~/.asdfrc
 
