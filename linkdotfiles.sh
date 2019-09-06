50a43d57ec9d465b557956924431d718e58702af
Jeff Langston <jrlangston@gmail.com>
2019-09-06 00:10:18 -0600

    Adding rofi config

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -7,6 +7,8 @@ ln -sf ~/dotfiles/.tmuxline.conf ~/.tmuxline.conf;
 ln -sf ~/dotfiles/.pgclirc ~/.config/pgcli/config;
 ln -sf ~/dotfiles/.myclirc ~/.myclirc
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/sway/config ~/.config/sway/config
+ln -sf ~/dotfiles/rofi/onedark.rasi ~/.config/rofi/onedark.rasi
 ln -sf ~/dotfiles/.asdfrc ~/.asdfrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
