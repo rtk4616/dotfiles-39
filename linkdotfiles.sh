822036883976221851af367fcf53c81d995dd5df
Jeff Langston <jrlangston@gmail.com>
2019-09-10 10:10:41 -0600

    i3 updates

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 mkdir -p ~/.config
-ln -sf ~/dotfiles/nvim ~/.config/nvim;
-ln -sf ~/dotfiles/.zshrc ~/.zshrc;
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf;
-ln -sf ~/dotfiles/.tmuxline.conf ~/.tmuxline.conf;
-ln -sf ~/dotfiles/.pgclirc ~/.config/pgcli/config;
+mkdir -p ~/.config/rofi
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmuxline.conf ~/.tmuxline.conf
+ln -sf ~/dotfiles/.pgclirc ~/.config/pgcli/config
 ln -sf ~/dotfiles/.myclirc ~/.myclirc
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
 ln -sf ~/dotfiles/sway/config ~/.config/sway/config
